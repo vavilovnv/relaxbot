f957310436dba918e2b9ce8c0015f5750d00dcb5
Nikolay <53932721+vavilovnv@users.noreply.github.com>
2022-06-19 18:24:43 +0300

    Requirements updated. Project structure changed. Annotating added.

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import logging
 import requests
 
 from random import randint
-from telegram import ReplyKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-
 from dotenv import load_dotenv
+from telegram.update import Update
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import (Updater, CommandHandler, MessageHandler,
+                          Filters, CallbackContext)
 
 load_dotenv()
 
@@ -22,8 +23,8 @@ URL_FOXES = 'https://randomfox.ca/images/'
 logger = logging.getLogger(__name__)
 
 
-def get_new_image(url, url_type):
-    """Получение ссылку на картинку."""
+def get_new_image(url: str, url_type: int) -> str:
+    """Getting a link to an image depending on the type of API."""
     if url_type == 1:
         try:
             response = requests.get(url)
@@ -40,35 +41,39 @@ def get_new_image(url, url_type):
             logger.error(f'Ошибка при запросе {url}: {error}')
 
 
-def send_new_cat(update, context):
-    """Отправка картинки с котиком."""
+def send_new_cat(update: Update, context: CallbackContext) -> None:
+    """Sending a picture of a cat"""
+    logger.info('Sending a picture with a cat')
     chat = update.effective_chat
     context.bot.send_photo(chat.id, get_new_image(URL_CATS, 1))
 
 
-def send_new_dog(update, context):
-    """Отправка картинки с собачкой."""
+def send_new_dog(update: Update, context: CallbackContext) -> None:
+    """Sending a picture of a dog."""
+    logger.info('Sending a picture with a dog')
     chat = update.effective_chat
     context.bot.send_photo(chat.id, get_new_image(URL_DOGS, 1))
 
 
-def send_new_fox(update, context):
-    """Отправка картинки с лисичкой."""
+def send_new_fox(update: Update, context: CallbackContext) -> None:
+    """Sending a picture of a fox."""
+    logger.info('Sending a picture with a fox')
     chat = update.effective_chat
     url_foxes = f'{URL_FOXES}{randint(1, 121)}.jpg'
     context.bot.send_photo(chat.id, get_new_image(url_foxes, 2))
 
 
-def send_text_message(update, context):
-    """Отправка текстового сообщения с напоминанием."""
+def send_text_message(update: Update, context: CallbackContext) -> None:
+    """Sending basic instruction."""
+    logger.info('Sending an instruction')
     chat = update.effective_chat
     text_message = (f'Нажимай скорее на кнопку c котиком, собачкой '
                     f'или лисичкой и я пришлю тебе картинку!')
     context.bot.send_message(chat_id=chat.id, text=text_message)
 
 
-def change_content_to_send(update, context):
-    """Выбор, какую картинку отправить."""
+def change_content_to_send(update: Update, context: CallbackContext) -> None:
+    """Select a picture to send."""
     message_text = update.message.text.strip()
     if message_text == 'Котик 🐱':
         send_new_cat(update, context)
@@ -80,8 +85,8 @@ def change_content_to_send(update, context):
         send_text_message(update, context)
 
 
-def wake_up_bot(update, context):
-    """Инициализация бота."""
+def wake_up_bot(update: Update, context: CallbackContext) -> None:
+    """Bot initialization."""
     chat = update.effective_chat
     name = chat.first_name
     keyboard = [
@@ -91,6 +96,7 @@ def wake_up_bot(update, context):
     hello_message = (f'Привет, {name}. Я релакс-бот. Помогу справится с '
                      f'плохим настроением. Нажимай на кнопку c котиком, '
                      f'собачкой или лисичкой и я пришлю тебе картинку.')
+    logger.info('Bot initialization')
     context.bot.send_message(
         chat_id=chat.id,
         text=hello_message,
@@ -99,12 +105,14 @@ def wake_up_bot(update, context):
 
 
 def main():
+    """The basis of the bot."""
     updater = Updater(token=TOKEN)
 
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('start', wake_up_bot))
     dp.add_handler(MessageHandler(Filters.text, change_content_to_send))
 
+    logger.info('Starting service...')
     updater.start_polling(poll_interval=RETRY_TIME)
     updater.idle()
 
